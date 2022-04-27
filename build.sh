a7a34d159c0113b5b9003a4d0c04636189892759
ivan-cx <i@ivn.cx>
2022-04-27 11:12:11 +0300

    new favicon and title

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 pandoc 404.md -s --css pandoc.css --html -o 404.html --metadata title="404 | ivan.cx"
 
-pandoc index.md -s --css pandoc.css --html -o index.html --metadata title="Reality driven software development | ivan.cx"
+pandoc index.md -s --css pandoc.css --html -o index.html --metadata title="ivan.cx"
 
